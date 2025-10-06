d2167642e5c024350d870c6080c438578baf1989
EunDuk2 <dmsqls6789@naver.com>
2025-10-07 01:01:33 +0900

    회원목록 화면 구현

--- chat-front/src/main.js
+++ chat-front/src/main.js
@@ -2,10 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from '@/router/index.js';
-
-// import axios from 'axios';
+import axios from 'axios';
 
 const app = createApp(App);
+
+axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token");
+        if(token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+)
 
 app.use(router);
 app.use(vuetify);
